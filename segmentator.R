7407e2d4b041209ae84d710a8ebcae06b39283cf
Jiayi Lu <215756347@qq.com>
2018-09-03 10:30:56 +0800

    Update segmentator.R

--- segmentator.R
+++ segmentator.R
@@ -1,5 +1,5 @@
 
-segmentator = function(x){
+x_segmentator = function(x){
   N = length(x)
   CURSOR = 1
   INDEX = 1
@@ -13,3 +13,39 @@ segmentator = function(x){
   }
   INDEX
 }
+
+interval_segmentator = function(x, MAX_INTERVAL_TIME = 60*60, RETURN = 'SEG', TYPE = 'time'){
+  N = length(x)
+  if(TYPE == 'time'){
+    x = lubridate::ymd_hms(x)
+    DIFF = as.numeric(as.duration(x[2:N] - x[1:(N-1)]))
+  } else {
+    DIFF = diff(x)
+  }
+  FIRSTS = which(DIFF > MAX_INTERVAL_TIME)
+  if(length(FIRSTS) == 0) {
+    L = 1:N
+    L2 = rep(1,N)
+  } else {
+    m = length(FIRSTS)
+    L = list()
+    for(i in 0:m){
+      if(i == 0){
+        INDEX = 1:(FIRSTS[1]-1)
+      } else if( i == m) {
+        INDEX = FIRSTS[m]:N
+      } else {
+        INDEX = FIRSTS[i]:(FIRSTS[i+1]-1)
+      }
+      L = rlist::list.append(L, INDEX)
+    }
+    LEN = length(L)
+    L2 = unlist(lapply(1:LEN, function(i) {rep(i, length(L[[i]]))}))
+  }
+  print('Return: SEG: segmentation vector, others: INDEX in a list')
+  print(paste('You have chosen Returning', RETURN))
+  if(RETURN == 'SEG') R = L2 else R = L
+  return(R)
+}
+
+
